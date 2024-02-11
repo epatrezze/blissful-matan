bd8f4b5c1ed2705d75401401dba60b165a45f64c
epatrezze <55457666+epatrezze@users.noreply.github.com>
2024-02-11 22:01:28 +0000

    botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./styles.css";
 
+window.Telegram.WebApp.MainButton.show();
+window.Telegram.WebApp.MainButton.setText("Alouuu");
+window.Telegram.WebApp.MainButton.onClick(() => {
+  window.Telegram.WebApp.showAlert("Alouuu");
+});
+
 export default function App() {
   return (
     <div className="App">
       <h1>Chupa Memo</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <img src="https://usagif.com/wp-content/uploads/2020/b72nv6/evrbddancen0w-49.gif" />
+
+      <div className="Body">
+        <img src="https://usagif.com/wp-content/uploads/2020/b72nv6/evrbddancen0w-49.gif" />
+      </div>
     </div>
   );
 }
