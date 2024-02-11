d823369ebf1184e7b937f3eb2c838368870a0de7
epatrezze <55457666+epatrezze@users.noreply.github.com>
2024-02-11 22:14:43 +0000

    funfander

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Telegram } from "@twa-dev/types";
+
+declare global {
+  interface Window {
+    Telegram: Telegram;
+  }
+}
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -8,5 +15,5 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
